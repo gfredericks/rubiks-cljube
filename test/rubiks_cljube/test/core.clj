6fcea0c9001bf7ccef1f8567bbec3780a22eb932
Gary Fredericks <fredericksgary@gmail.com>
2011-10-13 21:38:50 -0400

    Nother test

--- test/rubiks_cljube/test/core.clj
+++ test/rubiks_cljube/test/core.clj
@@ -18,4 +18,14 @@
        :D2 :D2
        :U2 :U2
        :F2 :F2
-       :B2 :B2))
+       :B2 :B2)
+  (let [move-seq
+        ; Wikipedia says this twists two corners
+          (map moves [:B :R' :D2 :R :B' :U2 :B :R' :D2 :R :B' :U2]),
+        {:keys [corner-pos edge-pos corner-or edge-or]}
+          (reduce #(%2 %1) solved move-seq)]
+    (is (= (:corner-pos solved) corner-pos))
+    (is (= (:edge-pos solved) edge-pos))
+    (is (= (:edge-or solved) edge-or))
+    (is (not= (:corner-or solved) corner-or))
+    (is (= 6 (count (filter #{0} corner-or))))))
